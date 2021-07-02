d830da876cd7aae59d0335f92bb8c34863f22f42
NdegwaG <ndegwa.skygo@gmail.com>
2021-07-02 10:22:35 +0300

    add array of male Akan names

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,3 @@
 document.getElementById("form").addEventListener("submit",formsubmitted);
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
